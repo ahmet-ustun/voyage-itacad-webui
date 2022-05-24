ce1194b855d2d35e87b97ee011f2a48e2e2a8195
ahmet-ustun <ahmetustunt@gmail.com>
2022-05-24 16:47:08 +0300

    adjusting the navbar in order to prepare for auth

--- src/app/components/static/navbar/navbar.component.ts
+++ src/app/components/static/navbar/navbar.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+	selector: 'app-navbar',
+	templateUrl: './navbar.component.html',
+	styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+	constructor(
+		public authService: AuthService,
+		public authFirebase: AngularFireAuth
+	) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
+	login() {
+		this.authService.googleLogin();
+	}
+
+	logout() {
+		this.authService.googleLogout();
+	}
 }
