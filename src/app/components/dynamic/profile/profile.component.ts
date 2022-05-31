29612f46d1c65f6e8167827de5e4eae2665f9bd5
ahmet-ustun <ahmetustunt@gmail.com>
2022-05-31 08:22:24 +0300

    creating a profile page with info

--- src/app/components/dynamic/profile/profile.component.ts
+++ src/app/components/dynamic/profile/profile.component.ts
@@ -1,15 +1,23 @@
+import { JsonPipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+	selector: 'app-profile',
+	templateUrl: './profile.component.html',
+	styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+	userData: any;
 
-  ngOnInit(): void {
-  }
+	constructor(
+		public authService: AuthService,
+	) {
+		this.userData = JSON.parse(localStorage.getItem('user')!);
+	}
+
+	ngOnInit(): void {
+	}
 
 }
