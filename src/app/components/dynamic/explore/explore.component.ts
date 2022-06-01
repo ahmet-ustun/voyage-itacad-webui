7aa0c7bf610adde744bc3b33db5752bd9464e2eb
asqasq333 <asq_asq33@abv.bg>
2022-06-01 20:40:46 +0300

    creating and making a style

--- src/app/components/dynamic/explore/explore.component.ts
+++ src/app/components/dynamic/explore/explore.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PlanetService } from 'src/app/services/planet/planet.service';
 
 @Component({
   selector: 'app-explore',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExploreComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public planetService: PlanetService
+  ) { }
 
   ngOnInit(): void {
   }
