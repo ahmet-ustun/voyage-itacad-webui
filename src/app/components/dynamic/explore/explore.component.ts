9a2759f6dcd54e80c9177b9df5038bf72d9d1771
ahmet-ustun <ahmetustunt@gmail.com>
2022-06-01 22:18:50 +0300

    some small adjustments

--- src/app/components/dynamic/explore/explore.component.ts
+++ src/app/components/dynamic/explore/explore.component.ts
@@ -2,17 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { PlanetService } from 'src/app/services/planet/planet.service';
 
 @Component({
-  selector: 'app-explore',
-  templateUrl: './explore.component.html',
-  styleUrls: ['./explore.component.css']
+	selector: 'app-explore',
+	templateUrl: './explore.component.html',
+	styleUrls: ['./explore.component.css']
 })
 export class ExploreComponent implements OnInit {
 
-  constructor(
-    public planetService: PlanetService
-  ) { }
+	constructor(
+		public planetService: PlanetService
+	) { }
 
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {
+	}
 }
