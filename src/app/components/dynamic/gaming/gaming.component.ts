c3b3ffbccac1a6be5ba45a3dba97fb833fbdcb6d
Selvi <selviam02@gmail.com>
2022-06-02 14:10:44 +0300

    some changes

--- src/app/components/dynamic/gaming/gaming.component.ts
+++ src/app/components/dynamic/gaming/gaming.component.ts
@@ -6,16 +6,4 @@ import { GamesService } from '../../../services/games/games.service';
   templateUrl: './gaming.component.html',
   styleUrls: ['./gaming.component.css'],
 })
-export class GamingComponent {
-  // gamesArrayCollection = [] as any;
-  // getData = async () => {
-  //   await this.gameService.gamesCollection.then((response: { docs: any[] }) => {
-  //     response.docs.map((doc) => this.gamesArrayCollection.push(doc.data())); //Get data, push each doc in new arr
-  //   });
-  //   console.log(this.gamesArrayCollection);
-  // };
-  // constructor(private gameService: GamesService) {}
-  // ngOnInit() {
-  //   this.getData();
-  // }
-}
+export class GamingComponent {}
