ed928444f37a48f16d9488bcbf259a711495c451
ahmet-ustun <ahmetustunt@gmail.com>
2022-06-06 20:29:34 +0300

    some changes

--- src/app/components/dynamic/gaming-child/gaming-child.component.ts
+++ src/app/components/dynamic/gaming-child/gaming-child.component.ts
@@ -1,11 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { slideInAnimation } from 'src/app/modules/app.animation';
 
 @Component({
 	selector: 'app-gaming-child',
 	templateUrl: './gaming-child.component.html',
-	styleUrls: ['./gaming-child.component.css'],
-	animations: [slideInAnimation]
+	styleUrls: ['./gaming-child.component.css']
 })
 export class GamingChildComponent implements OnInit {
 
