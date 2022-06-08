652d8574c90e498c5bbc606c78ff71e119862d33
ahmet-ustun <ahmetustunt@gmail.com>
2022-06-08 12:24:50 +0300

    adding the logic for species child router data

--- src/app/components/dynamic/species-child/species-child.component.ts
+++ src/app/components/dynamic/species-child/species-child.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { ISpecies } from 'src/app/interfaces/ispecies';
+import { StoreService } from 'src/app/services/store/store.service';
 
 @Component({
-  selector: 'app-species-child',
-  templateUrl: './species-child.component.html',
-  styleUrls: ['./species-child.component.css']
+	selector: 'app-species-child',
+	templateUrl: './species-child.component.html',
+	styleUrls: ['./species-child.component.css']
 })
-export class SpeciesChildComponent implements OnInit {
+export class SpeciesChildComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+	subscription$: any;
+	speciesId!: string;
+	speciesData!: ISpecies;
 
-  ngOnInit(): void {
-  }
+	constructor(
+		private route: ActivatedRoute,
+		private storeService: StoreService,
+		private toastr: ToastrService
+	) { }
 
+	ngOnInit(): void {
+		this.subscription$ = this.route.params.subscribe(params => {
+			this.speciesId = params['id'];
+			this.storeService.getSpecies(this.speciesId).valueChanges().subscribe({
+				next: data => this.speciesData = data,
+				error: error => this.toastr.error(error.message)
+			})
+		});
+	}
+
+	ngOnDestroy(): void {
+		this.subscription$.unsubscribe();
+	}
 }
