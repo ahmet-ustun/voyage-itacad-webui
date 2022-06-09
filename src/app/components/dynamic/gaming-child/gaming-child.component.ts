2d55c1c34dd1981d424d334d54aa4064bf758915
Selvi <selviam02@gmail.com>
2022-06-09 13:26:08 +0300

    conflict solving

--- src/app/components/dynamic/gaming-child/gaming-child.component.ts
+++ src/app/components/dynamic/gaming-child/gaming-child.component.ts
@@ -1,13 +1,37 @@
-import { getLocaleDateFormat } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+
+import { StoreService } from 'src/app/services/store/store.service';
 
 @Component({
-	selector: 'app-gaming-child',
-	templateUrl: './gaming-child.component.html',
-	styleUrls: ['./gaming-child.component.css'],
+  selector: 'app-gaming-child',
+  templateUrl: './gaming-child.component.html',
+  styleUrls: ['./gaming-child.component.css'],
 })
-export class GamingChildComponent implements OnInit {
-	constructor() { }
+export class GamingChildComponent implements OnInit, OnDestroy {
+  subscription$: any;
+  gamingId!: string;
+  gamingData: any;
+  constructor(
+    private route: ActivatedRoute,
+    private storeService: StoreService,
+    private toastr: ToastrService
+  ) {}
 
-	ngOnInit(): void { }
+  ngOnInit(): void {
+    this.subscription$ = this.route.params.subscribe((params) => {
+      this.gamingId = params['id'];
+      this.storeService
+        .getGames(this.gamingId)
+        .valueChanges()
+        .subscribe({
+          next: (data) => (this.gamingData = data),
+          error: (error) => this.toastr.error(error.message),
+        });
+    });
+  }
+  ngOnDestroy(): void {
+    this.subscription$.unsubscribe();
+  }
 }
