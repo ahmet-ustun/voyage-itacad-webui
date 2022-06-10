19f12a95b6fd47dbc6a7f9253b97be42e85bba94
Selvi <selviam02@gmail.com>
2022-06-10 14:21:54 +0300

    last one

--- src/app/services/games/games.service.ts
+++ src/app/services/games/games.service.ts
@@ -1,18 +1,16 @@
 import { Injectable } from '@angular/core';
-import { getFirestore } from "firebase/firestore";
-import { collection, getDocs } from "firebase/firestore"
+import { getFirestore } from 'firebase/firestore';
+import { collection, getDocs } from 'firebase/firestore';
 import { initializeApp } from 'firebase/app';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
-	providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class GamesService {
+  app = initializeApp(environment.firebase);
+  db = getFirestore(this.app);
+  gamesCollection = getDocs(collection(this.db, 'games'));
 
-	app = initializeApp(environment.firebase);
-	db = getFirestore(this.app);
-	gamesCollection = getDocs(collection(this.db, "games"));
-
-	constructor() { }
+  constructor() {}
 }
